da4dc0587ab38ab523e65187dc198d4e90add4ce
brent-soles <b.d.soles@gmail.com>
2019-04-01 17:49:33 -0700

    backend: see previous commit

--- reeych_backend/lib/reeych_backend_web/graphql/resolvers/accounts.ex
+++ reeych_backend/lib/reeych_backend_web/graphql/resolvers/accounts.ex
@@ -1,6 +1,6 @@
 defmodule ReeychBackendWeb.Graphql.AccountsResolver do
   alias ReeychBackend.Accounts
-  alias ReeychBackend.Accounts.{Credential}
+  alias ReeychBackend.Accounts.{Credential, User}
 
   def get_account(_root, %{email: email, password: password} = _args, _info) do
     case Accounts.get_account(%Credential{email: email, password: password}) do 
@@ -11,8 +11,16 @@ defmodule ReeychBackendWeb.Graphql.AccountsResolver do
     end
   end
 
-  def update_account(_root, args, _info) do
+  def update_account(_root, %{id: id} = args, _info) do
+    # Merge args into new structs to submit
+    # to update function. If no changes, ignore
+    # DB update
+    user_and_creds = Account.get_user!(id) |> Repo.preload(:credential)
+    
+    updates_user = struct(%User{}, args)
+    
+    updates_credential = struct(%Credential{}, args)
+  
 
   end
-
 end
